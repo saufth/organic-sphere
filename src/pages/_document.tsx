a87c08327271f5f9cb51bd5774db0b5815542aa1
Saufth <saufth@gmail.com>
2022-12-18 18:56:02 -0600

    Quotes to single fixed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document () {
   return (
-    <Html lang="es-MX">
+    <Html lang='es-MX'>
       <Head />
       <body>
         <Main />
